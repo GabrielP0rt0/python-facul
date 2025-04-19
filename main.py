40ba0f163ff10bec8d0d65371e37f6b3921bcae4
tc-gabriel-porto <tc.gabriel.porto@gruponc.net.br>
2025-04-18 22:55:07 -0300

    feat: add docker

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-"""
-Gerador de senhas aleatórias com comprimento personalizado.
-"""
-
+from fastapi import FastAPI
 import random
 import string
 
-def gerar_senha(comprimento=12):
-    """Gera uma senha aleatória com o comprimento especificado."""
+app = FastAPI()
+
+def gerar_senha(comprimento=16):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(caracteres) for _ in range(comprimento))
 
-if __name__ == "__main__":
-    try:
-        comprimento_usuario = int(input("Digite o comprimento desejado para a senha: "))
-        if comprimento_usuario <= 0:
-            raise ValueError("O comprimento deve ser maior que zero.")
-        SENHA_GERADA = gerar_senha(comprimento_usuario)
-        print(f"Sua senha gerada é: {SENHA_GERADA}")
-    except ValueError as erro:
-        print(f"Entrada inválida: {erro}")
+@app.get("/senha")
+def obter_senha():
+    senha = gerar_senha()
+    return {"senha": senha}
